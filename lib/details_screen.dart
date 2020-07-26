10aac932561a6e4dda08b51c33632ea095218fa8
UB <yyb2535@gmail.com>
2020-07-26 23:57:35 +0900

    flutter work Test

---
+++ lib/details_screen.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:food_app/constants.dart';
+
+class DetailsScreen extends StatefulWidget {
+  DetailsScreen({Key key}) : super(key: key);
+
+  DetailsState createState() => DetailsState();
+}
+
+class DetailsState extends State<DetailsScreen> {
+  int _count = 0;
+  increment() {
+    setState(() {
+      _count++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(
+          left: 20,
+          right: 20,
+          top: 50,
+        ),
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                SvgPicture.asset(
+                  "assets/icons/backward.svg",
+                  height: 11,
+                ),
+                SvgPicture.asset(
+                  "assets/icons/menu.svg",
+                  height: 11,
+                )
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 20),
+              height: 305,
+              width: 305,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: kSecondaryColor,
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/image_1_big.png"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "Vegan salad bowl",
+                        style: Theme.of(context).textTheme.title,
+                      ),
+                      TextSpan(
+                        text: "With red tomato",
+                        style: TextStyle(
+                          color: kTextColor.withOpacity(.5),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Text(
+                  "\$20",
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline
+                      .copyWith(color: kPrimaryColor),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+                "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem"),
+            Spacer(),
+            Padding(
+              padding: EdgeInsets.only(bottom: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: increment,
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 20, horizontal: 27),
+                      decoration: BoxDecoration(
+                        color: kPrimaryColor.withOpacity(.19),
+                        borderRadius: BorderRadius.circular(27),
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          Text(
+                            "Add to bag",
+                            style: Theme.of(context).textTheme.button,
+                          ),
+                          SizedBox(
+                            width: 30,
+                          ),
+                          SvgPicture.asset(
+                            "assets/icons/forward.svg",
+                            height: 11,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 75,
+                    width: 75,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: kPrimaryColor.withOpacity(.26),
+                    ),
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.all(15),
+                          height: 60,
+                          width: 60,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: kPrimaryColor,
+                          ),
+                          child: SvgPicture.asset("assets/icons/bag.svg"),
+                        ),
+                        Positioned(
+                          right: 15,
+                          bottom: 10,
+                          child: Container(
+                            alignment: Alignment.center,
+                            height: 28,
+                            width: 28,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: kWhiteColor,
+                            ),
+                            child: Text(
+                              "${_count}",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .button
+                                  .copyWith(color: kPrimaryColor, fontSize: 15),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
